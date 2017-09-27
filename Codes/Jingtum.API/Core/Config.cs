49d6237f77f9bb2999c8aa70480ec23dd2e793de
foreso-GitHub <31951179+foreso-GitHub@users.noreply.github.com>
2017-09-27 23:11:05 +0800

    Add files via upload

---
+++ Codes/Jingtum.API/Core/Config.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jingtum.API.Core
+{
+    class Config
+    {
+        public const String DEFAULT_ALPHABET = "jpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCg65rkm8oFqi1tuvAxyz";
+
+        private static B58IdentiferCodecs m_B58IdentiferCodecs;
+
+        public static void SetAlphabet(String alphabet)
+        {
+            B58 b58 = new B58(alphabet);
+            m_B58IdentiferCodecs = new B58IdentiferCodecs(b58);
+        }
+
+        public static B58IdentiferCodecs GetB58IdentiferCodecs() 
+        {
+            SetAlphabet(DEFAULT_ALPHABET);
+            return m_B58IdentiferCodecs;
+        }
+
+        /***
+         * We set up all the defaults here
+         */
+        //static {
+        //    setAlphabet(DEFAULT_ALPHABET);
+        //}
+    }
+}
