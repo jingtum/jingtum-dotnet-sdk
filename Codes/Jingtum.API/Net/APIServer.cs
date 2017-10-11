d1c2db997165c9ac1017f1f3fbfd13c97d9958c3
foreso-GitHub <31951179+foreso-GitHub@users.noreply.github.com>
2017-10-11 21:44:51 +0800

    Add files via upload

--- Codes/Jingtum.API/Net/APIServer.cs
+++ Codes/Jingtum.API/Net/APIServer.cs
@@ -13,23 +13,31 @@ namespace Jingtum.API.Net
     class APIServer
     {
         #region consts
-        public const string SIGN_BACK_SLASH = "/";
-        public const string SIGN_QUESTION_MARK = "?";
+        public const string SIGN_BACKSLASH = "/";
+        public const string SIGN_QUESTION = "?";
         public const string SIGN_AND = "&";
         public const string SIGN_ADD = "+";
+        public const string SIGN_EQUAL = "=";
 
         public const string URL_SERVER_ADDRESS = "https://api.jingtum.com/";
         public const string URL_SERVER_VERSION = "v2";
+
+        //https://api.jingtum.com/v2/
+        public const string URL_SERVER_ADDRESS_VERSION =
+            URL_SERVER_ADDRESS
+            + URL_SERVER_VERSION
+            + SIGN_BACKSLASH;
         
         //https://api.jingtum.com/v2/accounts/
-        public const string URL_SERVER_ADDRESS_ACCOUNT = 
-            URL_SERVER_ADDRESS 
-            + URL_SERVER_VERSION
-            + SIGN_BACK_SLASH
+        public const string URL_SERVER_ADDRESS_ACCOUNT =
+            URL_SERVER_ADDRESS_VERSION
             + "accounts"
-            + SIGN_BACK_SLASH;
-        
-        public const string Request_Url_ResultsPerPage = "results_per_page";
+            + SIGN_BACKSLASH;
+
+        //public const string PARAMETER_RESULTS_CURRENCY = "currency";
+        //public const string PARAMETER_RESULTS_ISSUER = "issuer";
+        //public const string PARAMETER_RESULTS_PER_PAGE = "results_per_page";
+        //public const string PARAMETER_PAGE = "page";
         //public const string Request_Url_Marker = "marker={ledger:" + ledger + ",seq:" + seq + "};
         //public const string Request_Url_Marker = "marker={ledger:{0},seq:{1}}";
 
@@ -121,29 +129,139 @@ namespace Jingtum.API.Net
         #region format url methods
         public string FormatURL(string serverURL, string address, string type, string parameters)
         {
-            return serverURL 
-                + (!(serverURL.EndsWith(SIGN_BACK_SLASH)) ? SIGN_BACK_SLASH : string.Empty) 
-                + address 
-                + SIGN_BACK_SLASH 
+            return serverURL
+                + (!(serverURL.EndsWith(SIGN_BACKSLASH)) ? SIGN_BACKSLASH : string.Empty) 
+                + address
+                + SIGN_BACKSLASH 
                 + type 
                 + parameters;
         }
 
         public string FormatURL(string address, string type, string parameters)
         {
-            return URL_SERVER_ADDRESS_ACCOUNT 
-                + address 
-                + SIGN_BACK_SLASH 
-                + type 
-                + parameters;
+            return FormatURL(URL_SERVER_ADDRESS_ACCOUNT, address, type, parameters);
         }
 
         public string FormatURL(string address, string type)
         {
-            return URL_SERVER_ADDRESS_ACCOUNT 
-                + address 
-                + SIGN_BACK_SLASH 
-                + type;
+            return FormatURL(address, type, string.Empty);
+        }
+
+        private static string FormatParameter(string existingParameter, string parameterName, string value)
+        {
+            if(value != string.Empty)
+            {
+                return FormatParameter(existingParameter, parameterName + APIServer.SIGN_EQUAL + value);
+            }
+
+            return existingParameter;
+        }
+
+        private static string FormatParameter(string existingParameter, string newParameter)
+        {
+            if (newParameter != string.Empty)
+            {
+                if (existingParameter == string.Empty)//mean this is the first parameter, need start from "?".
+                {
+                    existingParameter += APIServer.SIGN_QUESTION;
+                }
+                else//not the  first parameter, need start from "&".
+                {
+                    existingParameter += APIServer.SIGN_AND;
+                }
+
+                existingParameter += newParameter;
+            }
+
+            return existingParameter;
+        }
+
+        public static string FormatParameter_Currency(string existingParameter, string currency)
+        {
+            if (currency != string.Empty)
+            {
+                if (Utility.IsValidCurrency(currency))
+                {
+                    existingParameter = APIServer.FormatParameter(existingParameter, "currency", currency);
+                }
+                else
+                {
+                    throw new InvalidParameterException(JingtumMessage.INVALID_CURRENCY, currency);
+                }
+            }
+
+            return existingParameter;
+        }
+
+        public static string FormatParameter_Issuer(string existingParameter, string issuer)
+        {
+            if (issuer != string.Empty)
+            {
+                if (Utility.IsValidAddress(issuer))
+                {
+                    existingParameter = APIServer.FormatParameter(existingParameter, "issuer", issuer);
+                }
+                else
+                {
+                    throw new InvalidParameterException(JingtumMessage.INVALID_JINGTUM_ADDRESS, issuer);
+                }
+            }
+
+            return existingParameter;
+        }
+
+        public static string FormatParameter_Currency_CounterParty(string currency, string counterParty)
+        {
+            if (Utility.IsValidCurrency(currency))
+            {
+                if (counterParty == string.Empty)
+                {
+                    return currency;
+                }
+                else if (Utility.IsValidAddress(counterParty))
+                {
+                    return currency + Net.APIServer.SIGN_ADD + counterParty;
+                }
+                else
+                {
+                    throw new InvalidParameterException(JingtumMessage.INVALID_JINGTUM_ADDRESS, counterParty);
+                }                
+            }
+            else
+            {
+                throw new InvalidParameterException(JingtumMessage.INVALID_CURRENCY, currency);
+            }
+        }
+
+        public static string FormatParameter_Result_Per_Page(string existingParameter, int results_per_page, int page)
+        {
+            if (results_per_page > 0)
+            {
+                existingParameter = APIServer.FormatParameter(existingParameter, "results_per_page", results_per_page.ToString());
+            }
+
+            if (page > 0)
+            {
+                existingParameter = APIServer.FormatParameter(existingParameter, "page", page.ToString());
+            }
+
+            return existingParameter;
+        }
+
+        public static string FormatParameter_Hash(string hash)
+        {
+            return Net.APIServer.SIGN_BACKSLASH + hash;
+        }
+
+        public static string FormatParameter_Marker(string existingParameter, int ledger, int seq)
+        {
+            if (ledger > 0)
+            {
+                string parameter = "marker={ledger:" + ledger + ",seq:" + seq + "}";
+                existingParameter = APIServer.FormatParameter(existingParameter, parameter);
+            }
+
+            return existingParameter;
         }
         #endregion
     }
