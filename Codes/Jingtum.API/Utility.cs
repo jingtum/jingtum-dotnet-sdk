1d4a53c1b2e4fe3aee7d6c160228f2cdafb9ff6f
foreso-GitHub <31951179+foreso-GitHub@users.noreply.github.com>
2017-11-06 23:03:04 +0800

    Add simple check for secret.

--- Codes/Jingtum.API/Utility.cs
+++ Codes/Jingtum.API/Utility.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -35,6 +35,10 @@ namespace Jingtum.API
 
         public static bool IsValidSecret(string secret)
         {
+            if(secret == null || secret == string.Empty)
+            {
+                return false;
+            }
             return true;
 
             //to be implemented.
